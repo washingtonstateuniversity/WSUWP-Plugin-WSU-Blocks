ab34ff31338e93fa1b6818e8ef672975aaf0bea0
jeremyfelt <jeremy.felt@gmail.com>
2014-04-10 14:09:50 -0700

    Setup initial metabox for adding various block sizes to content
    Our intial options will be single, double, and triple sizes. These
    are really just tied to a class name in the WSU Spine, so are
    fairly arbitrary. If we do things right, these can be manipulated
    easily through the use of a filter.

--- wsuwp-blocks.php
+++ wsuwp-blocks.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WSUWP Blocks
+Plugin Name: WSU Blocks
 Version: 0.0.1
 Plugin URI: http://web.wsu.edu
 Description: Manage blocks of content in WordPress.
@@ -8,9 +8,29 @@ Author: washingtonstateuniversity, jeremyfelt
 Author URI: http://web.wsu.edu
 */
 
-class WSUWP_Blocks {
+class WSU_Blocks {
 	public function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 10, 1 );
+	}
 
+	/**
+	 * Add custom meta boxes to the edit post screen.
+	 *
+	 * @param string $post_type The post type of the current editor view.
+	 */
+	public function add_meta_boxes( $post_type ) {
+		add_meta_box( 'wsublocks-add', 'Add Content Block', array( $this, 'display_add_content_block' ), 'page', 'normal', 'default' );
+	}
+
+	/**
+	 * Display a meta box with options for adding various block sizes to the page.
+	 */
+	public function display_add_content_block() {
+		?>
+		<div id="add-wsublock-single" class="wsublock-add">Add a single wide block</div>
+		<div id="add-wsublock-double" class="wsublock-add">Add two even blocks</div>
+		<div id="add-wsublock-triple" class="wsublock-add">Add three even blocks</div>
+		<?php
 	}
 }
-new WSUWP_Blocks();
+new WSU_Blocks();
