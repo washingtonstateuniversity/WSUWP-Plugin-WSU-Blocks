a7b5eaaa7eee9688add79797ebb2a91db454d77d
jeremyfelt <jeremy.felt@gmail.com>
2014-04-10 16:08:12 -0700

    Add documentation to JS, start with a proper 0 count

--- js/wsublock-admin.js
+++ js/wsublock-admin.js
@@ -1,19 +1,34 @@
 (function($ ){
 
+	/**
+	 * Contains the default settings for a new tinyMCE instance.
+	 */
 	var tiny_instance_defaults;
 
-	var tiny_instance_count = 1;
+	/**
+	 * Contains the current count of tinyMCE boxes displayed on
+	 * the screen.
+	 *
+	 * @type {number}
+	 */
+	var tiny_instance_count = 0;
 
+	/**
+	 * Process a click on an add content block box.
+	 *
+	 * @param element_id string containing the clicked element.
+	 */
 	handleClick = function( element_id ) {
 		if ( 'add-wsublock-single' === element_id ) {
+			tiny_instance_count++;
 			window.tinyMCEPreInit.mceInit['wsublock-current-' + tiny_instance_count ] = tiny_instance_defaults;
 			window.tinyMCEPreInit.mceInit['wsublock-current-' + tiny_instance_count ].selector = '#wsublock-current-' + tiny_instance_count;
 			window.tinyMCEPreInit.mceInit['wsublock-current-' + tiny_instance_count ].body_class = window.tinyMCEPreInit.mceInit['wsublock-current-' + tiny_instance_count ].body_class.replace( 'wsublock-hidden-temp', 'wsublock-current-' + tiny_instance_count );
 			window.tinymce.init( window.tinyMCEPreInit.mceInit['wsublock-current-' + tiny_instance_count ] );
-			tiny_instance_count++;
 		}
 	};
 
+	// We can immediately attach a click event, as this exists in the DOM already.
 	$( '#wsublocks-add' ).on( 'click', '.wsublock-add', function() { handleClick( this.id ) } );
 
 	$(document ).ready( function(){
