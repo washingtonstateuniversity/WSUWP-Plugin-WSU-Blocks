26bbd33f187c4deccc95a9d60e04979cf0c19765
jeremyfelt <jeremy.felt@gmail.com>
2014-04-10 11:02:37 -0700

    Add initial plugin file for WSUWP Blocks

---
+++ wsuwp-blocks.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: WSUWP Blocks
+Version: 0.0.1
+Plugin URI: http://web.wsu.edu
+Description: Manage blocks of content in WordPress.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
+
+class WSUWP_Blocks {
+	public function __construct() {
+
+	}
+}
+new WSUWP_Blocks();
